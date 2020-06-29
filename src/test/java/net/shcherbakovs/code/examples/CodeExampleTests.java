bca1458db9d3358ca5f34bb30534e579401855e2
Sergey Shcherbakov <sergey.shcherbakov@gmail.com>
2020-06-30 01:06:35 +0200

    Initial commit

---
+++ src/test/java/net/shcherbakovs/code/examples/CodeExampleTests.java
@@ -0,0 +1,19 @@
+package net.shcherbakovs.code.examples;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CodeExampleTests {
+
+	@Test
+	public void testMRU() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void testLRU() {
+		assertTrue(true);
+	}
+
+}
